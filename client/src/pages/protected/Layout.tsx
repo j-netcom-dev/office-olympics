b2bf8229db568b09210362364acb673c12c2010f
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-07-26 01:24:36 +0300

    chore: modified navbar size

--- client/src/pages/protected/Layout.tsx
+++ client/src/pages/protected/Layout.tsx
@@ -11,11 +11,11 @@ const Layout = () => {
 
     return (
         <div className="min-h-[100svh] bg-zinc-200 flex flex-col gap-8">
-            <header className="shadow-sm flex items-center justify-between px-8 bg-white">
+            <header className="flex items-center justify-between px-8 bg-white">
                 <strong className="text-xl font-bold uppercase text-blue-600 cursor-default">Office Olympics</strong>
                 <nav className="flex gap-8 items-center uppercase">
-                    <NavLink to={'/'} className="py-4 text-base font-semibold hover:text-blue-500 transition-all">Dashboard</NavLink>
-                    <NavLink to={'/leaderboard'} className="py-4 text-base font-semibold hover:text-blue-500 transition-all">Leaderboard</NavLink>
+                    <NavLink to={'/'} className="py-5 text-base font-semibold hover:text-blue-500 transition-all">Dashboard</NavLink>
+                    <NavLink to={'/leaderboard'} className="py-5 text-base font-semibold hover:text-blue-500 transition-all">Leaderboard</NavLink>
                 </nav>
             </header>
             <Outlet />
