713ea33c571726e6536c3dad91ceee00054014f6
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-07-25 17:39:59 +0300

    feat: created a service to interract with the user model

---
+++ api/services/user-service.js
@@ -0,0 +1,20 @@
+const User =require('../models/user-model.js');
+const { BAD_REQUEST, CREATED, CONFLICT } =require('../constants.js')
+
+class UserService{
+
+    static create =async data =>{
+        const {first_name, last_name} =data
+        try {
+            const userFound =await User.findOne({first_name, last_name})
+            if(userFound) return {status: CONFLICT, message: `Name: ${first_name} ${last_name} already taken`}
+            const user =new User({first_name, last_name});
+            await user.save()
+            return {status: CREATED, message: 'Created'}
+        } catch ({message}) {
+            return {status: BAD_REQUEST, message}
+        }
+    }
+}
+
+module.exports ={ UserService };
