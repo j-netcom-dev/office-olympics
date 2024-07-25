7d0da401a943fd74ba1194906058c3dfe85fea0d
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-07-25 23:08:50 +0300

    chore: added routes for private pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { PublicLayout, RegisterPage } from './pages/public';
+import { Dashboard, LeaderBoard, ProtectedLayout } from './pages/protected';
 
 const App =() =>{
 
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element ={<ProtectedLayout />}>
+          <Route index element ={<Dashboard />}/>
+          <Route path='leaderboard' element ={<LeaderBoard />}/>
+        </Route>
         <Route path='/register' element ={<PublicLayout />}>
           <Route index element ={<RegisterPage />}/>
         </Route>
