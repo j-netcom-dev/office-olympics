4c303f0183ac5d164a3cfc2f4912d5cbf1c7ce42
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-07-25 23:10:14 +0300

    feat: created a navigation bar

---
+++ client/src/pages/protected/Layout.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { read_from_storage } from "../../storage";
+import {  NavLink, Outlet, useNavigate } from "react-router-dom";
+
+const Layout = () => {
+    const navigation =useNavigate()
+    useEffect(() =>{
+        const data =read_from_storage('user');
+        if(!data || !data.userid) navigation('/register', {replace: true})
+    }, []);
+
+    return (
+        <div className="min-h-[100svh] bg-">
+            <header className="shadow-sm flex items-center justify-between px-8">
+                <strong className="text-xl font-bold uppercase text-blue-600 cursor-default">Office Olympics</strong>
+                <nav className="flex gap-8 items-center uppercase">
+                    <NavLink to={'/'} className="py-4 text-base font-semibold hover:text-blue-500 transition-all">Dashboard</NavLink>
+                    <NavLink to={'/leaderboard'} className="py-4 text-base font-semibold hover:text-blue-500 transition-all">Leaderboard</NavLink>
+                </nav>
+            </header>
+            <Outlet />
+        </div>
+    )
+}
+
+export default Layout;
