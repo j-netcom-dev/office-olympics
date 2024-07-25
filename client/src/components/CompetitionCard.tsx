6da055453282164432fff6a57320cf7c1f2882ea
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-07-26 01:58:39 +0300

    chore: changed link

--- client/src/components/CompetitionCard.tsx
+++ client/src/components/CompetitionCard.tsx
@@ -4,7 +4,7 @@ const CompetitionCard = ({id, name}: {name: string, id: string}) => {
   return (
     <div className="shadow bg-white flex flex-col gap-8 h-max px-4 py-8 rounded-xl border">
       <h2 className="font-semibold text-xl">{name}</h2>
-      <NavLink className={'block bg-blue-500 text-white text-center py-2 px-4 rounded'} to={`/participants/${id}`}>Open</NavLink>
+      <NavLink className={'block bg-blue-500 text-white text-center py-2 px-4 rounded'} to={`/admin/competition/${id}`}>Open</NavLink>
     </div>
   )
 }
