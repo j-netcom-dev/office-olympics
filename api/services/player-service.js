742659396440a1d67dd880dd61bf833944c41ca9
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-07-25 18:26:18 +0300

    feat: implemented crud handler for players

---
+++ api/services/player-service.js
@@ -0,0 +1,29 @@
+const Player =require('../models/player-model.js');
+const { BAD_REQUEST, CREATED, CONFLICT, OKAY, SERVER_ERROR } =require('../constants.js')
+
+class PlayerService{
+
+    static create =async data =>{
+        const {first_name, last_name, gender, nationality} =data
+        try {
+            const playerFound =await Player.findOne({first_name, last_name})
+            if(playerFound) return {status: CONFLICT, message: `Player name ${first_name} ${last_name} already taken`}
+            const player =new Player({first_name, last_name, gender, nationality});
+            await player.save()
+            return {status: CREATED, message: 'Created'}
+        } catch ({message}) {
+            return {status: BAD_REQUEST, message}
+        }
+    }
+
+    static fetch = async () =>{
+        try {
+            const players =await Player.find();
+            return {status: OKAY, players}
+        } catch ({message}) {
+            return {status: SERVER_ERROR, message}
+        }
+    }
+}
+
+module.exports ={ PlayerService };
