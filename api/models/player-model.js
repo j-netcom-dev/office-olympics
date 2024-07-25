782296ad07f01780fe3a90ea1ea6849695a1260c
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-07-25 18:29:56 +0300

    refactor: removed image field

--- api/models/player-model.js
+++ api/models/player-model.js
@@ -5,7 +5,6 @@ const schema =mongoose.Schema({
     last_name: { type: String, required: true },
     gender: { type: String, required: true },
     nationality: { type: String, required: true },
-    image: { type: String, required: true },
 }, { versionKey: false });
 
 const Player =mongoose.model('player', schema);
