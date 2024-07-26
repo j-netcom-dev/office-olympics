278b009414c659416f4f32490d4e327816e0ed28
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-07-26 19:14:20 +0300

    fix: fixed multiple predictions on one game issue

--- api/services/prediction-service.js
+++ api/services/prediction-service.js
@@ -1,11 +1,13 @@
 const Prediction =require('../models/prediction-model.js');
-const { BAD_REQUEST, CREATED, OKAY, SERVER_ERROR } =require('../constants.js')
+const { BAD_REQUEST, CREATED, OKAY, SERVER_ERROR, CONFLICT } =require('../constants.js')
 
 class PredictionService{
 
     static create =async data =>{
         const { user, competition, winner } =data
         try {
+            const competitionFound =await Prediction.findOne({competition});
+            if(competitionFound) return {status: CONFLICT, message:  `You already made predictions for this competition`}
             const prediction =new Prediction({user, competition, winner });
             await prediction.save()
             return {status: CREATED, message: 'Saved'}
