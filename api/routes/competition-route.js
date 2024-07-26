f77c719470d9c4a79c81e7caa764a75d667e543f
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-07-26 05:26:41 +0300

    feat: created endpoints for declaring winner and adding players to competitions

--- api/routes/competition-route.js
+++ api/routes/competition-route.js
@@ -1,10 +1,11 @@
 const { Router } =require('express');
-const { create, list, get } =require('../controllers/competition-controller.js')
+const { create, list, get, updateParticipants, setWinner } =require('../controllers/competition-controller.js')
 
 const router =Router();
 
 router.post('/', create);
 router.get('/list', list);
 router.get('/get/:_id', get);
-
+router.put('/put/:competition_id/:participant_id', updateParticipants)
+router.put('/set/winner/:competition_id/:participant_id', setWinner)
 module.exports =router;
