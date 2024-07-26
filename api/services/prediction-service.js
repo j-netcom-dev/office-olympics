3ff78ae86bf4cb37880ef83562044e74d6592244
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-07-26 21:09:43 +0300

    fix: competition prediction

--- api/services/prediction-service.js
+++ api/services/prediction-service.js
@@ -6,7 +6,7 @@ class PredictionService{
     static create =async data =>{
         const { user, competition, winner } =data
         try {
-            const competitionFound =await Prediction.findOne({competition});
+            const competitionFound =await Prediction.findOne({competition, user});
             if(competitionFound) return {status: CONFLICT, message:  `You already made predictions for this competition`}
             const prediction =new Prediction({user, competition, winner });
             await prediction.save()
@@ -19,14 +19,8 @@ class PredictionService{
     static fetch = async () =>{
         try {
             const payload =await Prediction.find()
+            .populate('competition')
             .populate('user')
-            .populate({
-                path: 'competition',
-                populate: {
-                    path: 'winer',
-                    model: 'user'
-                }
-            })
             .populate('winner');
             return {status: OKAY, payload}
         } catch ({message}) {
