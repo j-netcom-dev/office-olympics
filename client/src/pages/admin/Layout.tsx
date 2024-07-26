d6bbc6c7db1828d59e35ceac49620dda1bc0a3a6
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-07-26 03:19:28 +0300

    chore: added navbar to the layout

--- client/src/pages/admin/Layout.tsx
+++ client/src/pages/admin/Layout.tsx
@@ -1,8 +1,17 @@
-import { Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <Outlet />
+    <div className="bg-zinc-100 min-h-[100svh]">
+            <header className="flex items-center justify-between px-8">
+                <strong className="text-xl font-bold uppercase text-blue-600 cursor-default">Office Olympics</strong>
+                <nav className="flex gap-8 items-center uppercase">
+                    <NavLink end to={'/admin'} className="py-5 text-base font-semibold hover:text-blue-500 transition-all">Home</NavLink>
+                    <NavLink to={'/admin/players'} className="py-5 text-base font-semibold hover:text-blue-500 transition-all">Competitors</NavLink>
+                </nav>
+            </header>
+            <Outlet />
+        </div>
   )
 }
 
